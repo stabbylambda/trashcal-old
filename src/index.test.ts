53dbc423dd77e02ea1f46511c51a4d56f1af60d6
David Stone <david@stabbylambda.com>
2022-05-10 19:59:22 -0700

    Add format

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,11 @@
 import { trashcal } from "./";
 
-test('alltypes', async () => {
-    const result = await trashcal({ id: 'alltypes' });
-    expect(result.length()).toBe(3)
-})
+test("alltypes", async () => {
+  const result = await trashcal({ id: "alltypes" });
+  expect(result.length()).toBe(3);
+});
 
-test('noteligible', async () => {
-    const result = await trashcal({ id: 'noteligible' });
-    expect(result.length()).toBe(2)
-})
+test("noteligible", async () => {
+  const result = await trashcal({ id: "noteligible" });
+  expect(result.length()).toBe(2);
+});
--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,13 +1,16 @@
-import { setupServer } from 'msw/node'
-import { rest } from 'msw';
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
-const handler = rest.get('https://getitdone.force.com/CollectionDetail', (req, res, ctx) => {
-    const id = req.url.searchParams.get('id');
-    console.log('loading mock for', id)
-    const html = readFileSync(resolve(`./src/mocks/${id}.html`), 'utf-8');
+const handler = rest.get(
+  "https://getitdone.force.com/CollectionDetail",
+  (req, res, ctx) => {
+    const id = req.url.searchParams.get("id");
+    console.log("loading mock for", id);
+    const html = readFileSync(resolve(`./src/mocks/${id}.html`), "utf-8");
     return res(ctx.xml(html));
-});
+  }
+);
 
 export const server = setupServer(handler);
